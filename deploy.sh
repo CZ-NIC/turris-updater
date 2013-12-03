6631740025d0b6564209e30999c1ea9598e1e48a
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-03 16:09:42 +0100

    A script to deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+# TODO: Check the git is signed by known pgp and there is a signed-off-by
+while read source target hash ; do
+	current_hash=$(cat $source/git-hash)
+	if [ "$hash" != "$current_hash" ] ; then
+		echo "The hash on $source/$target does not match ‒ $hash vs. $current_hash"
+		exit 1
+	fi
+	rm -rf "$target"
+	cp -pr "$source" "$target"
+	scp -r "$target" "api.turris.cz:$target-upload"
+	ssh api.turris.cz "chmod a+rX '$target-upload' -R && mv 'openwrt-repo/$target' '$target-rm' && mv '$target-upload' 'openwrt-repo/$target' && rm -rf '$target-rm'"
+done
