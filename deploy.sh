4890f48143752b570e45eadd9d9dbbbe6243dafd
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-12 10:21:04 +0100

    The key is in place, enable checking when deploying

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-if false ; then # Disable the safety check until we have it properly deployed
 # First, check this commit is signed. That means we are allowed to proceed with deployment.
-if [ "$(git log -n1 --pretty='%G?')" != 'G' ] ; then
+if [ "$(git log -n1 --pretty='%G?' updater/deploy.sh updater/deploy)" != 'G' ] ; then
 	echo 'The last commit on misc is not signed by trusted key, not continuing'
 	exit 1
 fi
 
-if [ "$(git log -n1 | grep 'Signed-off-by' | wc -l)" -lt 2 ] ; then
+if [ "$(git log -n1 updater/deploy.sh updater/deploy | grep 'Signed-off-by' | wc -l)" -lt 2 ] ; then
 	echo 'The last commit is not signed off by at least two people, not continuing'
 	exit 1
-fi
 fi
 
 cd "$HOME"/turris-packages
