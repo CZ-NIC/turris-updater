6a2fcfe91087d6ebfe417609a951098dba2b8acb
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2014-09-10 13:27:39 +0200

    Don't include downloaded packages in notifications

--- updater-worker.sh
+++ updater-worker.sh
@@ -232,7 +232,7 @@ run_plan() {
 
 gen_notifies() {
 	if [ -s "$LOG_FILE" ] ; then
-		timeout 120 create_notification -s update "$(sed -e 's/^I \(.*\) \(.*\)/ • Nainstalovaná verze \2 balíku \1/;s/^R \(.*\)/ • Odstraněn balík \1/' "$LOG_FILE")"
+		timeout 120 create_notification -s update "$(sed -ne 's/^I \(.*\) \(.*\)/ • Nainstalovaná verze \2 balíku \1/p;s/^R \(.*\)/ • Odstraněn balík \1/p' "$LOG_FILE")"
 	fi
 	timeout 120 notifier || echo 'Notifier failed' | my_logger -p daemon.error
 }
