d5e4050eb1d92df3a43574ae87ee93b140adf8d3
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-01-29 12:40:18 +0100

    arguments: Provide response to the commands
    Be able to terminate correctly, be able to print help. Inform about the
    other operations not being implemented yet.

--- src/opkg-trans/main.c
+++ src/opkg-trans/main.c
@@ -20,10 +20,38 @@
 #include "../lib/arguments.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+const char *help =
+"opkg-trans -j			Recover from a crash/reboot from a journal.\n"
+"opkg-trans -b			Abort interrupted work in the journal and clean.\n"
+"				up. Some stages of installation might not be\n"
+"				aborted.\n"
+"opkg-trans -a pkg1.opkg -r pkg2	Install and remove packages. The ones to install\n"
+"				(-a) need a path to already downloaded package\n"
+"				file. The ones to remove (-r) expect name of the\n"
+"				package.\n"
+"opkg-trans -h			This help message.\n";
 
 int main(int argc, char *argv[]) {
 	struct cmd_op *ops = cmd_args_parse(argc, argv);
-	// TODO: Perform the operations
+	struct cmd_op *op = ops;
+	for (; op->type != COT_EXIT && op->type != COT_CRASH; op ++)
+		switch (op->type) {
+			case COT_HELP:
+				fputs(help, stderr);
+				break;
+				// Some not implemented operations
+#define NIP(TYPE) case COT_##TYPE: fputs("Operation " #TYPE " not implemented yet\n", stderr); return 1
+				NIP(JOURNAL_ABORT);
+				NIP(JOURNAL_RESUME);
+				NIP(INSTALL);
+				NIP(REMOVE);
+			default:
+				assert(0);
+		}
+	enum cmd_op_type exit_type = op->type;
 	free(ops);
-	return 0;
+	return exit_type == COT_EXIT ? 0 : 1;
 }
