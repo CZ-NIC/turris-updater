2a4e9d8d29a83651b5808f61819dcd2268b5e79f
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-02-01 11:04:22 +0100

    Fix copyright headers
    This is not nuci configuration server (copy-pasted from there).

--- tests/ctest.h
+++ tests/ctest.h
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016, CZ.NIC z.s.p.o. (http://www.nic.cz/)
  *
- * This file is part of NUCI configuration server.
+ * This file is part of the turris updater.
  *
- * NUCI is free software: you can redistribute it and/or modify
+ * Updater is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- * NUCI is distributed in the hope that it will be useful,
+ * Updater is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with NUCI.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Updater.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
