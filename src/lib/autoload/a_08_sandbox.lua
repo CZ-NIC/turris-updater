8ce05980d72cb565840b444287d747d89d507e37
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-04-05 11:28:14 +0200

    sandbox: Run a sandboxed chunk

--- src/lib/autoload/a_08_sandbox.lua
+++ src/lib/autoload/a_08_sandbox.lua
@@ -45,7 +45,7 @@ local rest_available_funcs = {
 	type,
 	unpack,
 	xpcall
-};
+}
 
 -- Functions to be injected into an environment in the given security level
 local funcs = {
@@ -81,6 +81,15 @@ utils.table_merge(funcs.Remote, funcs.Restricted)
 utils.table_merge(funcs.Local, funcs.Remote)
 utils.table_merge(funcs.Full, funcs.Local)
 
+--[[
+Create a new context. The context inherits everything
+from its parent (if the parent is not nil). The security
+level is set to the one given (if nil is given, it is also
+inherited).
+
+A new environment, corresponding to the security level,
+is constructed and stored in the result as „env“.
+]]
 function new(sec_level, parent)
 	local result = {}
 	--[[
@@ -106,6 +115,62 @@ function new(sec_level, parent)
 		end
 	end
 	return result
+end
+
+--[[
+Run a given chunk in a sandbox.
+
+The chunk, if it is a string, is compiled (and given the name). If it is a function,
+it is run directly.
+
+A new context is created, using the new() function. Everything that is in the
+context_merge table is added to the context (not the environment contained therein).
+The context_mod is run with the context as a parameter. Both context_merge and
+context_mod may be nil, in which case nothing is modified.
+
+The result describes success of the run. If nil is returned, everything went well.
+Otherwise, an error description structure is returned. It looks something like this:
+{
+	tp = "error",
+	reason = <reason>,
+	msg = <human readable description>
+}
+
+The <reason> is one of:
+ • "compilation": The compilation (eg. loading) of the chunk failed.
+ • "runtime": An error has been caught at run time.
+ • Something else ‒ anything is allowed to throw these structured exceptions and they
+   are simply passed.
+]]
+function run_sandboxed(chunk, name, sec_level, parent, context_merge, context_mod)
+	if type(chunk) == "string" then
+		local err
+		chunk, err = loadstring(chunk, name)
+		if not chunk then
+			return {
+				tp = "error",
+				reason = "compilation",
+				msg = err
+			}
+		end
+	end
+	local context = new(sec_level, parent)
+	utils.table_merge(context, context_merge or {})
+	context_mod = context_mod or function () end
+	context_mod(context)
+	local func = setfenv(func, context.env)
+	local ok, err = pcall(func)
+	if not ok then
+		if type(err) == "table" and err.tp == "error" then
+			return err
+		else
+			return {
+				tp = "error",
+				reason = "runtime",
+				msg = err
+			}
+		end
+	end
 end
 
 return _M
