b245b08e39237927741023de204a4e83eebe908a
Robin Obůrka <robin.oburka@nic.cz>
2016-04-13 21:16:48 +0200

    events: Add some download documentation

--- src/lib/events.h
+++ src/lib/events.h
@@ -148,6 +148,10 @@ struct wait_id run_command_a(struct events *events, command_callback_t callback,
 struct wait_id download(struct events *events, download_callback_t callback, void *data, const char *url, const char *cacert, const char *crl) __attribute__((nonnull(1, 2, 4)));
 /*
  * Set the number of maximum parallel downloads
+ *
+ * If the value is set to a smaller number than currently running downloads
+ * the downloads are finished as usual. New download from queue is started
+ * when a free download slot is available.
  */
 void download_slot_count_set(struct events *event, size_t count) __attribute__((nonnull(1)));
 
