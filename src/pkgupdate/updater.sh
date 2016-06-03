91dc5623c041c2c23622f5323b276a0374a6391b
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-06-03 10:53:54 +0200

    updater.sh: Better locking & state manipulation
    Lock the state manipulation, correct it in case the updater terminates
    incorrectly, etc.

--- src/pkgupdate/updater.sh
+++ src/pkgupdate/updater.sh
@@ -2,10 +2,31 @@
 
 get-api-crl
 
+STATE_DIR=/tmp/update-state
+LOCK_DIR="$STATE_DIR/lock"
+EXIT_CODE=1
+
+# Prepare a state directory and lock
 mkdir -p /tmp/update-state
-pkgupdate file:///etc/updater/entry.lua --batch
-# This manipulation is not safe against concurrent runs of updater.sh. But the damage in such case is small (just a wrong status visible in nuci for a while) and it should happen only rarely, so we ignore it.
-STATE=$(cat /tmp/update-state/state)
-if [ "$STATE" != "done" ] && [ "$STATE" != "error" ] ; then
-	echo lost >/tmp/update-state/state
+trap 'rm -rf "$LOCK_DIR"; exit "$EXIT_CODE"' EXIT INT QUIT TERM ABRT
+if ! mkdir "$LOCK_DIR" ; then
+	echo "Already running" >&2
+	echo "Already running" | my_logger -p daemon.warning
+	EXIT_CODE=0
+	exit
 fi
+cat /dev/null >"$STATE_DIR"/log2
+
+# Run the actual updater
+pkgupdate file:///etc/updater/entry.lua --batch
+# Evaluate what has run
+EXIT_CODE="$?"
+STATE=$(cat "$STATE_DIR"/state)
+if [ "$EXIT_CODE" != "0" ] && [ "$STATE" != "error" ] ; then
+	echo lost >"$STATE_DIR"/state
+fi
+if [ "$STATE" != "done" ] && [ "$STATE" != "error" ] ; then
+	echo lost >"$STATE_DIR"/state
+fi
+
+# Let the trap clean up here
