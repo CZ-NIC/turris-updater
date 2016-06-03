b4cc1357d2b2e7779503362645ad82ab7c7df14c
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-06-03 11:26:45 +0200

    updater.sh: Send notifications

--- src/pkgupdate/updater.sh
+++ src/pkgupdate/updater.sh
@@ -4,6 +4,7 @@ get-api-crl
 
 STATE_DIR=/tmp/update-state
 LOCK_DIR="$STATE_DIR/lock"
+LOG_FILE="$STATE_DIR/log2"
 EXIT_CODE=1
 BACKGROUND=false
 BACKGROUNDED=false
@@ -32,7 +33,8 @@ if ! $BACKGROUNDED ; then
 		EXIT_CODE=0
 		exit
 	fi
-	cat /dev/null >"$STATE_DIR"/log2
+	cat /dev/null >"$LOG_FILE"
+	echo startup >"$STATE_DIR/state"
 fi
 if $BACKGROUND ; then
 	"$0" -r >/dev/null 2>&1 &
@@ -53,5 +55,18 @@ fi
 if [ "$STATE" != "done" ] && [ "$STATE" != "error" ] ; then
 	echo lost >"$STATE_DIR"/state
 fi
+
+if [ -s "$STATE_DIR"/log2 ] ; then
+	create_notification -s update "$(sed -ne 's/^I \(.*\) \(.*\)/ • Nainstalovaná verze \2 balíku \1/p;s/^R \(.*\)/ • Odstraněn balík \1/p' "$LOG_FILE")" "$(sed -ne 's/^I \(.*\) \(.*\)/ • Installed version \2 of package \1/p;s/^R \(.*\)/ • Removed package \1/p' "$LOG_FILE")" || echo "Create notification failed" | logger -p daemon.error
+fi
+if [ "$EXIT_CODE" != 0 ] || [ "$STATE" != "done" ] ; then
+	if [ -s "$STATE_DIR/last_error" ] ; then
+		ERROR=$(cat "$STATE_DIR/last_error")
+	else
+		ERROR="Unknown error"
+	fi
+	create_notification -s error "Updater selhal: $ERROR" "Updater failed: $ERROR" || echo "Create notification failed" | logger -p daemon.error
+fi
+notifier || echo "Notifier failed" | logger -p daemon.error
 
 # Let the trap clean up here
