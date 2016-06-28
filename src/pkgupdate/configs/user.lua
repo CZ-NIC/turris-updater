4a91143761a3ffcb20b0ce820ffcc93b85db4329
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-06-28 16:17:53 +0200

    Example fix: That comma doesn't work

--- src/pkgupdate/configs/user.lua
+++ src/pkgupdate/configs/user.lua
@@ -2,5 +2,5 @@
 A place for user definitions.
 
 Repository "name" "URI" { ca = "file:///etc/ssl/ca.pem", pubkey = "file:///etc/repo.pubkey" }
-Install "pkgname", "other"
+Install "pkgname" "other"
 ]]
