765844cd5e0f0a82ee909b9ce8fd1c2ac7c2127d
Michal Vaner <michal.vaner@nic.cz>
2016-07-14 17:52:05 +0200

    updater.sh: Timeouts
    Limit the time for which certain commands (including the main pkgupdate)
    are allowed to run.

--- src/pkgupdate/updater.sh
+++ src/pkgupdate/updater.sh
@@ -34,6 +34,47 @@ PID_FILE="$STATE_DIR/pid"
 EXIT_CODE=1
 BACKGROUND=false
 BACKGROUNDED=false
+TMP_DIR="/tmp/$$.tmp"
+
+timeout() {
+	# Let a command run for up to $1 seconds. If it doesn't finishes by then, kill it.
+	# The timeout starts it in background. Also, a watcher process is started that'd kill
+	# it after the timeout and waits for it to finish. If the program finishes, it kills
+	# the watcher.
+	TIME="$1"
+	PROG="$2"
+	shift 2
+	set +e
+	"$PROG" "$@" >"$TMP_DIR"/t-output &
+	export CPID="$!"
+	(
+		trap 'kill $SLEEP ; exit' TERM INT QUIT ABRT
+		sleep "$TIME" &
+		SLEEP=$!
+		wait $SLEEP
+		echo "Killing $PROG/$CPID after $TIME seconds (stuck?)" | logger -t updater -p daemon.error
+		kill "$CPID"
+		sleep 5 &
+		SLEEP=$!
+		wait $SLEEP
+		kill -9 "$CPID"
+		# Wait to be killed by the parrent
+		sleep 60 &
+		SLEEP=$!
+		wait $SLEEP
+	) &
+	WATCHER="$!"
+	wait "$CPID"
+	RESULT="$?"
+	CNT=0
+	while [ "$CNT" -lt 5 ] && ! kill "$WATCHER" ; do
+		sleep 1
+		CNT=$((CNT+1))
+	done
+	wait "$WATCHER"
+	set -e
+	return "$RESULT"
+}
 
 while [ "$1" ] ; do
 	case "$1" in
@@ -69,10 +110,11 @@ if $BACKGROUND ; then
 	echo $!>"$PID_FILE"
 	exit
 fi
-trap 'rm -rf "$LOCK_DIR" "$PID_FILE"; exit "$EXIT_CODE"' EXIT INT QUIT TERM ABRT
+mkdir -p "$TMP_DIR"
+trap 'rm -rf "$LOCK_DIR" "$PID_FILE" "$TMP_DIR"; exit "$EXIT_CODE"' EXIT INT QUIT TERM ABRT
 
 # Run the actual updater
-UPDATER_ENABLE_STATE_LOG=true pkgupdate file:///etc/updater/entry.lua --batch
+UPDATER_ENABLE_STATE_LOG=true timeout 900 pkgupdate file:///etc/updater/entry.lua --batch
 # Evaluate what has run
 EXIT_CODE="$?"
 STATE=$(cat "$STATE_DIR"/state)
@@ -84,7 +126,7 @@ if [ "$STATE" != "done" ] && [ "$STATE" != "error" ] ; then
 fi
 
 if [ -s "$STATE_DIR"/log2 ] ; then
-	create_notification -s update "$(sed -ne 's/^I \(.*\) \(.*\)/ • Nainstalovaná verze \2 balíku \1/p;s/^R \(.*\)/ • Odstraněn balík \1/p' "$LOG_FILE")" "$(sed -ne 's/^I \(.*\) \(.*\)/ • Installed version \2 of package \1/p;s/^R \(.*\)/ • Removed package \1/p' "$LOG_FILE")" || echo "Create notification failed" | logger -t updater -p daemon.error
+	timeout 120 create_notification -s update "$(sed -ne 's/^I \(.*\) \(.*\)/ • Nainstalovaná verze \2 balíku \1/p;s/^R \(.*\)/ • Odstraněn balík \1/p' "$LOG_FILE")" "$(sed -ne 's/^I \(.*\) \(.*\)/ • Installed version \2 of package \1/p;s/^R \(.*\)/ • Removed package \1/p' "$LOG_FILE")" || echo "Create notification failed" | logger -t updater -p daemon.error
 fi
 if [ "$EXIT_CODE" != 0 ] || [ "$STATE" != "done" ] ; then
 	if [ -s "$STATE_DIR/last_error" ] ; then
@@ -92,8 +134,8 @@ if [ "$EXIT_CODE" != 0 ] || [ "$STATE" != "done" ] ; then
 	else
 		ERROR="Unknown error"
 	fi
-	create_notification -s error "Updater selhal: $ERROR" "Updater failed: $ERROR" || echo "Create notification failed" | logger -t updater -p daemon.error
+	timeout 120 create_notification -s error "Updater selhal: $ERROR" "Updater failed: $ERROR" || echo "Create notification failed" | logger -t updater -p daemon.error
 fi
-notifier || echo "Notifier failed" | logger -t updater -p daemon.error
+timeout 120 notifier || echo "Notifier failed" | logger -t updater -p daemon.error
 
 # Let the trap clean up here
