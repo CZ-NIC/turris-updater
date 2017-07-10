5bc67f4cc8bfe39da90ec7e25a7c4bf162300f86
Karel Kočí <karel.koci@nic.cz>
2017-07-10 06:58:30 +0200

    Remove legacy version and migrator
    All routers are now migrated (or at least should be). So we no longer
    need legacy version of updater nor we need migrator. This commit removes
    both of these code bases. Previous commit is tagged with
    legacy-migrator for easier lookup.

--- tests/system/auto-migrate-pkgs/output/config.lua
+++
@@ -1,2 +0,0 @@
--- Auto-migration performed (do not delete this line, or it may attempt doing so again)
-Install "extra-pkg" { ignore = { "missing" } }
--- tests/system/auto-migrate-pkgs/output/usr/bin/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "Test script installed"
