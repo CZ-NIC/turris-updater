920c0aa1d7e2a4290a7d3c4674a04c0bd3be7e99
Karel Kočí <karel.koci@nic.cz>
2018-02-13 17:31:02 +0100

    Add possibility to specify environment variables to subprocess

--- tests/subprocess.lua
+++ tests/subprocess.lua
@@ -22,27 +22,33 @@ require 'lunit'
 module("subproc", package.seeall, lunit.testcase)
 
 function test_exit_code()
-	local ok, out = subprocess(LST_HOOK, "Test: true", 1, "true")
+	local ok, out = subprocess(LST_HOOK, "Test: true", 1, {}, "true")
 	assert_equal(0, ok)
 	assert_equal("", out)
 
-	local ok, out = subprocess(LST_HOOK, "Test: false", 1, "false")
+	local ok, out = subprocess(LST_HOOK, "Test: false", 1, {}, "false")
 	assert_not_equal(0, ok)
 	assert_equal("", out)
 end
 
 function test_output()
-	local ok, out = subprocess(LST_HOOK, "Test: echo", 1, "echo", "hello")
+	local ok, out = subprocess(LST_HOOK, "Test: echo", 1, {}, "echo", "hello")
 	assert_equal(0, ok)
 	assert_equal("hello\n", out)
 
-	local ok, out = subprocess(LST_HOOK, "Test: echo stderr", 1, "sh", "-c", "echo hello >&2")
+	local ok, out = subprocess(LST_HOOK, "Test: echo stderr", 1, {}, "sh", "-c", "echo hello >&2")
 	assert_equal(0, ok)
 	assert_equal("hello\n", out)
 end
 
 function test_timeout()
-	local ok, out = subprocess(LST_HOOK, "Test: sleep", 1, "sleep", "2")
+	local ok, out = subprocess(LST_HOOK, "Test: sleep", 1, {}, "sleep", "2")
 	assert_not_equal(0, ok)
 	assert_equal("", out)
+end
+
+function test_env()
+	local ok, out = subprocess(LST_HOOK, "Test: env", 1, {['TESTMSG'] = 'hello'}, "sh", "-c", "echo $TESTMSG")
+	assert_equal(0, ok)
+	assert_equal("hello\n", out)
 end
