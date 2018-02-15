1c7e08ba4c728d7632d37f2c932ae411c2e63017
Karel Kočí <karel.koci@nic.cz>
2018-02-15 13:28:10 +0100

    Suppress some warnings

--- src/lib/journal.c
+++ src/lib/journal.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017, CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright 2016-2018, CZ.NIC z.s.p.o. (http://www.nic.cz/)
  *
  * This file is part of the turris updater.
  *
@@ -139,10 +139,12 @@ static bool journal_open(lua_State *L, int flags) {
 		switch (errno) {
 			case EEXIST:
 				luaL_error(L, "Unfinished journal exists");
+				FALLTROUGH;
 			case ENOENT:
 				if (!(flags & O_CREAT))
 					return false;
 				// Otherwise ‒ fall through to the default section
+				FALLTROUGH;
 			default:
 				luaL_error(L, "Error opening journal: %s", strerror(errno));
 		}
