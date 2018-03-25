198b2e0d57b9eea155e7d666903e48c92ff81649
Karel Kočí <cynerd@email.cz>
2018-03-25 15:08:32 +0200

    Fix migration to supervisor
    When updater is updated then foris can approve another execution but
    because it's not yet updated it runs updater.sh. But because we don't
    have that installed yet we run pkgupdate directly instead.
    
    This has side effect that migration update won't report changes to
    notification system. But that should be fine.

--- src/supervisor/updater.sh
+++ src/supervisor/updater.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -t 1 ]; then
+if [ -t 1 ] || ! which updater-supervisor 2>/dev/null >&2; then
 	echo "updater.sh is obsoleted. Please use pkgupdate directly instead." >&2
 	pkgupdate --batch "$@"
 else
