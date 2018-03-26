fa0bc56210291ce89bdf34def31064f99772c989
Karel Kočí <karel.koci@nic.cz>
2018-03-26 11:12:36 +0200

    Fallback to pkgupdate when -b is passed as argument
    Durring migration a -b -c is passed to updater.sh as arguments. This
    causes pkgupdate to fail. We should filter those out and if -b is
    specified we should run updater-supervisor instead. But there is a case
    when we don't have it installed yet and because of that we can allso
    fallback to pkgupdate directly.

--- src/supervisor/updater.sh
+++ src/supervisor/updater.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
 
-if [ -t 1 ] || ! which updater-supervisor 2>/dev/null >&2; then
+SUPERVISOR_ARGS="--wait-for-network"
+
+WIPE=true
+for ARG in "$@"; do
+	if $WIPE; then
+		shift $#
+		WIPE=false
+	fi
+
+	case "$ARG" in
+		--rand-sleep)
+			SUPERVISOR_ARGS="$SUPERVISOR_ARGS --rand-sleep"
+			;;
+		-b|--background|-w|-n)
+			echo "Argument $ARG ignored." >&2
+			;;
+		*)
+			set "$@" "$ARG"
+			;;
+	esac
+done
+
+if [ -t 1 ] ; then
 	echo "updater.sh is obsoleted. Please use pkgupdate directly instead." >&2
-	pkgupdate --batch "$@"
+	if $BACKGROUND; then
+		pkgupdate --batch "$@" &
+	else
+		pkgupdate --batch "$@"
+	fi
 else
-	updater-supervisor -d
+	if which updater-supervisor 2>/dev/null >&2; then
+		updater-supervisor -d
+	else
+		pkgupdate --batch &
+	fi
 fi
