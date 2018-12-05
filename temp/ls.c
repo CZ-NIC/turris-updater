3d6e3adcd3adb6431bcb95ef4ce25d8cf9a6c9f4
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-12-05 12:55:29 +0100

    Code is more robust, handles links properly

--- temp/ls.c
+++ temp/ls.c
@@ -5,30 +5,33 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+struct tree_funcs {
+	int (*file_func)(const char *);
+	int (*dir_func)(const char *, int);
+};
 
-int foreach_file(const char *dirname, int (*file_func)(const char *), int (*dir_func)(const char *, int)) {
-/*int foreach_file(const char *dirname) {*/
+int foreach_file(const char *dirname, struct tree_funcs funcs) {
 /*
-
-FIXME: It needs to check if <directory> exists, otherwise it crashes!
 
 TODO: Handle links 
 	- links to files are copied as files
 	- links to dirs are copied as links
 
-TODO: DIR_FUNC() needs to be called twice - befor entering and after leaving
-
 */
 
 	/*printf("dir to read: %s\n", dirname);*/
 
-	struct stat sb;
+	struct stat fileinfo;
+	struct stat linkinfo;
 	struct dirent **namelist;
-	int n;
+	int n, fret;
+
 	n = scandir(dirname, &namelist, NULL, alphasort);
-	if (n < 0)
+/*	printf("n:%d\n",n);**/
+	if (n < 0) {
+		printf("***PROBLEM with %s***\n", dirname);
 		perror("scandir");
-	else {
+	} else {
 		while(n--) {
 			/* Ignore "." and ".." */
 			if(
@@ -43,21 +46,32 @@ TODO: DIR_FUNC() needs to be called twice - befor entering and after leaving
 				if(dirname[strlen(dirname) - 1] != '/')
 					strcat(fullpath, "/");
 				strcat(fullpath, namelist[n]->d_name);
-				if(lstat(fullpath, &sb) == 0) {
-					/* check file type */
-					switch (sb.st_mode & S_IFMT) {
-						case S_IFDIR:
-							dir_func(fullpath, 0);
-							foreach_file(fullpath, file_func, dir_func);
-							dir_func(fullpath, 1);
-							break;
-						case S_IFREG:
-							file_func(fullpath);
-							break;
-					}
-				} else {
-					/* file doesn't exist, most probably */
-					printf("some problem\n");
+				/* get info about both file and it's target, if it's link */
+				lstat(fullpath, &linkinfo);
+				fret = stat(fullpath, &fileinfo);	
+				if(fret == -1) {
+					/* file does not exist */
+					printf("File %s does not exist.\n", fullpath);
+				} else if(S_ISREG(linkinfo.st_mode)) {
+					/* regular file */
+					printf("File %s is regular file.\n", fullpath);
+					funcs.file_func(fullpath);
+				} else if(S_ISLNK(linkinfo.st_mode)) {
+					/* link to file */
+					printf("File %s is link to file.\n", fullpath);
+				} else if(S_ISDIR(linkinfo.st_mode)) {
+					/* directory */
+					printf("%s is directory.\n", fullpath);
+					funcs.dir_func(fullpath, 0);
+					foreach_file(fullpath, funcs);
+					funcs.dir_func(fullpath, 1);
+				} else if(S_ISDIR(fileinfo.st_mode)) {
+					/* link to directory */
+					printf("%s is link to directory.\n", fullpath);
+					/* NOTE: Should we treat link to dir differently? */
+					funcs.dir_func(fullpath, 0);
+					foreach_file(fullpath, funcs);
+					funcs.dir_func(fullpath, 1);
 				}
 				
 				/* cleanup */
@@ -65,10 +79,13 @@ TODO: DIR_FUNC() needs to be called twice - befor entering and after leaving
 			}
 			free(namelist[n]);
 		}
+		free(namelist);
 	}
-	free(namelist);
 	return(0);
 }
+
+/* --- PRINT TREE --- */
+
 
 int dir_depth = 0;
 const char *dir_prefix = "--------------------"; /* max allowed depth is 20 dirs, enough for testing */
@@ -78,8 +95,6 @@ int print_file(const char *name) {
 	printf("F:%s:%s\n", prefix, name);
 	return 0;
 }
-
-/* NOTE: TYPE is 0 on enter and 1 on leave */
 int print_dir(const char *name, int type) {
 	if (type == 0) {
 		dir_depth += 1;
@@ -93,10 +108,18 @@ int print_dir(const char *name, int type) {
 	return 0;
 }
 
+struct tree_funcs print_tree = {
+	print_file,
+	print_dir
+};
+
+/* --- end of tree --- */
+
 int main(int argc, char **argv) {
 	char *dirname = argv[1];
 	printf("-------------\n");
-	foreach_file(dirname, print_file, print_dir);
+/*	foreach_file(dirname, print_file, print_dir);*/
+	foreach_file(dirname, print_tree);
 	printf("-------------\n");
 	return(0);
 }
