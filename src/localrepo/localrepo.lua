91cbf95eb99509e7461ce030ac37c3447fe49ec9
Karel Kočí <cynerd@email.cz>
2018-12-07 16:33:26 +0100

    localrepo: improve updater lua script
    This makes localrepo.lua work with root_dir to be usable outside of current
    root.  It also does not inserts localrepo.lua script unless it exists on
    file system. This hides unnecessary warning that confuses users.

--- src/localrepo/localrepo.lua
+++ src/localrepo/localrepo.lua
@@ -2,5 +2,8 @@
 This file is part of updater-ng-localrepo. Don't edit it.
 ]]
 
--- Add local repositories (might be missing if not installed or used)
-Script("localrepo", "file:///usr/share/updater/localrepo/localrepo.lua", { ignore = { "missing" } })
+-- Add local repositories (might be missing if not in use)
+script_path = "file://" .. root_dir .. "usr/share/updater/localrepo/localrepo.lua"
+if stat(script_path) == "f" then
+	Script("localrepo", script_path)
+end
