bcae0cbf0a13b15448018bf9c837754938e0ed38
Karel Kočí <karel.koci@nic.cz>
2018-12-12 15:43:00 +0100

    TMP

--- tests/subprocess.lua
+++ tests/subprocess.lua
@@ -21,49 +21,55 @@ require 'lunit'
 
 module("subproc", package.seeall, lunit.testcase)
 
+--[[
+On Debian lunit for some reason prints when invoked in subprocess information
+about executed test suite again. This of course taints start of subprocess.
+This function removes known problematic two first lines from beginning of output
+if pattern is detected.
+]]
+local function fix_out(out)
+	if out:find('Loaded testsuite with', 1, true) ~= nil then
+		local _, last = out:find('\n\n', 1, true)
+		return out:sub(last)
+	end
+	return out
+end
+
 function test_exit_code()
 	local ok, out = subprocess(LST_HOOK, "Test: true", 1000, "true")
 	assert_equal(0, ok)
-	assert_equal("", out)
+	assert_equal("", fix_out(out))
 
 	local ok, out = subprocess(LST_HOOK, "Test: false", 1000, "false")
 	assert_not_equal(0, ok)
-	assert_equal("", out)
+	assert_equal("", fix_out(out))
 end
 
 function test_output()
 	local ok, out = subprocess(LST_HOOK, "Test: echo", 1000, "echo", "hello")
 	assert_equal(0, ok)
-	assert_equal("hello\n", out)
+	assert_equal("hello\n", fix_out(out))
 
 	local ok, out = subprocess(LST_HOOK, "Test: echo stderr", 1000, "sh", "-c", "echo hello >&2")
 	assert_equal(0, ok)
-	assert_equal("hello\n", out)
+	assert_equal("hello\n", fix_out(out))
 end
 
 function test_timeout()
 	subprocess_kill_timeout(0)
 	local ok, out = subprocess(LST_HOOK, "Test: sleep", 1000, "sleep", "2")
 	assert_not_equal(0, ok)
-	assert_equal("", out)
+	assert_equal("", fix_out(out))
 end
 
 function test_callback()
 	subprocess_kill_timeout(0)
 
-	--[[
-	Note: We intentionally use here callback to just set environment variable.
-	Correct functionality of stdout and stderr from callback is tested in
-	subprocess.c test suite. We have to just test if we are able to execute lua
-	code in callback.
-	It is not tested with plain print because test suite seems to somehow detect
-	fork and prints test statistics like on beginning of whole tests run. This
-	is pretty annoying and solution is to simply not test output. Also note that
-	this is only test suite problem so real functionality is not affected and
-	prints can be used in callbacks like normal.
-	]]
-	local ok, out = subprocess(LST_HOOK, "Test: env", 1000, function () setenv("TESTENV", "Hello env") end, "sh", "-c", "echo $TESTENV")
+	local ok, out = subprocess(LST_HOOK, "Test: env", 1000, function () io.stderr:write("Hello callback") end, "true")
 	assert_equal(0, ok)
-	assert_equal("Hello env\n", out)
+	assert_equal("Hello callback", fix_out(out))
+	local ok, out = subprocess(LST_HOOK, "Test: env", 1000, function () setenv("TESTENV", "Hello env callback") end, "sh", "-c", "echo $TESTENV")
+	assert_equal(0, ok)
+	assert_equal("Hello env callback\n", fix_out(out))
 end
 
