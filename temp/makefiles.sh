56811b8dc92201558d93a2b60aebf5d50c2ac46d
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-12-12 14:32:53 +0100

    Tests improved

--- temp/makefiles.sh
+++ temp/makefiles.sh
@@ -1,9 +1,11 @@
 mkdir dir
 mkdir dir/subdir1
 mkdir dir/subdir2
-echo hello > dir/file1
+echo hello1 > dir/file1
+echo hello2 > dir/file2
 echo hello > dir/subdir1/file1
 ln -s dir/file1 dir/link1
 ln -s dir/subdir1 dir/linkdir1
 echo gotcha > dir/subdir1/file_to_find
+echo gotcha > dir/subdir1/another_file_to_find
 
---
+++ temp/test.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+./makefiles.sh
+./rls dir
