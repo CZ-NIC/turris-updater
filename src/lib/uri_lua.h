7a031666712fb92ec2387efa02e05ba92046d51b
Karel Kočí <karel.koci@nic.cz>
2019-01-23 10:02:05 +0100

    TMP

---
+++ src/lib/uri_lua.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019, CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This file is part of the Turris Updater.
+ *
+ * Updater is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Updater is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Updater.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef UPDATER_URI_LUA_H
+#define UPDATER_URI_LUA_H
+
+#include <lua.h>
+
+// Create uri module and inject it into the lua state
+void uri_mod_init(lua_State *L) __attribute__((nonnull));
+
+#endif
