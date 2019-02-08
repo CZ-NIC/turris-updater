585aec3eee1437a17be46eb84116ef5b54acaf5a
Karel Kočí <cynerd@email.cz>
2019-02-08 10:16:41 +0100

    localrepo: fix script existence detection

--- src/localrepo/localrepo.lua
+++ src/localrepo/localrepo.lua
@@ -3,7 +3,7 @@ This file is part of updater-ng-localrepo. Don't edit it.
 ]]
 
 -- Add local repositories (might be missing if not in use)
-script_path = "file://" .. root_dir .. "usr/share/updater/localrepo/localrepo.lua"
-if stat(script_path) == "f" then
-	Script("localrepo", script_path)
+script_path = root_dir .. "usr/share/updater/localrepo/localrepo.lua"
+if stat(script_path) ~= "" then
+	Script("localrepo", "file://" .. script_path)
 end
