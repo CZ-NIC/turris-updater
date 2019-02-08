62cd6f4be54b77a29ecfaf0b5b4707649e2fa0fa
Karel Kočí <karel.koci@nic.cz>
2019-02-08 15:21:53 +0100

    localrepo: fix previous fix
    Previous commit fixed problem for existing localrepo.lua but introduced
    an error for non-existing script.

--- src/localrepo/localrepo.lua
+++ src/localrepo/localrepo.lua
@@ -4,6 +4,6 @@ This file is part of updater-ng-localrepo. Don't edit it.
 
 -- Add local repositories (might be missing if not in use)
 script_path = root_dir .. "usr/share/updater/localrepo/localrepo.lua"
-if stat(script_path) ~= "" then
+if stat(script_path) then
 	Script("localrepo", "file://" .. script_path)
 end
