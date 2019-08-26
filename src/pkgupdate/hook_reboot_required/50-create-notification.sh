2166034b07549ef96552948cf8c93b7452eace87
Karel Kočí <karel.koci@nic.cz>
2019-08-26 13:04:21 +0200

    pkgupdate: do not create reboot notification for non-root system
    This skips content of create-notification reboot_required hook in case
    if path to system is not /. Only currently running system should raise
    reboot_required request.

--- src/pkgupdate/hook_reboot_required/50-create-notification.sh
+++ src/pkgupdate/hook_reboot_required/50-create-notification.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-create_notification -s restart "Systém byl aktualizován, ale některé změny se projeví až po restartu. Prosím restartujte své zařízení." "The system was updated, but some changes will take effect only after reboot. Please reboot the device." || echo "Create notification failed"
+# Do not try to create notification for run on non-root system
+[ "$ROOT_DIR" = "/" ] || exit 0
+
+create_notification -s restart \
+	"Systém byl aktualizován, ale některé změny se projeví až po restartu. Prosím restartujte své zařízení." \
+	"The system was updated, but some changes will take effect only after reboot. Please reboot the device." \
+		|| echo "Create notification failed"
