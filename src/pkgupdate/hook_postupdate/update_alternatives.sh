bfd4acf352e106397d430a4f7087387680f3e43d
Karel Kočí <karel.koci@nic.cz>
2019-08-26 13:01:29 +0200

    pkgupdate: update alternatives hook fix to use ROOT_DIR
    This fixes problem where alternatives were not correctly created in
    system run in non-root mode. This was because as source the global path
    was used but in places like build system this path is invalid. We have
    those files but relative to ROOT_DIR path. This fixes that.
    
    This also just fixes small shell syntax problem where paths were no
    quoted.

--- src/pkgupdate/hook_postupdate/update_alternatives.sh
+++ src/pkgupdate/hook_postupdate/update_alternatives.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sed -n 's/^Alternatives://p' /usr/lib/opkg/info/*.control | \
+sed -n 's/^Alternatives://p' "$ROOT_DIR"/usr/lib/opkg/info/*.control | \
 	tr , '\n' | \
 	sed 's/^\ \([^:]*\):\([^:]*\):/\2:\1:/' | \
 	sort | \
 	while IFS=: read TRG PRIO SRC; do
-		ln -sf $SRC $TRG
+		ln -sf "$SRC" "$TRG"
 	done
