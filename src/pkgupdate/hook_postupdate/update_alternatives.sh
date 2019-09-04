8467e68f94a5e46916d29d8030eb359699ad4ac0
Karel Kočí <karel.koci@nic.cz>
2019-09-04 17:15:17 +0200

    pkgupdate: create links to alternatives in correct path
    The previous fix correctly changed source of alternatives but it was
    still trying to create link in invalid location (ignoring configured
    root). This adds this path in appropriate location.
    
    Note that TRG is an absolute path so there does not have to be any slash
    between ROOT_DIR and TRG.

--- src/pkgupdate/hook_postupdate/update_alternatives.sh
+++ src/pkgupdate/hook_postupdate/update_alternatives.sh
@@ -4,5 +4,5 @@ sed -n 's/^Alternatives://p' "$ROOT_DIR"/usr/lib/opkg/info/*.control | \
 	sed 's/^\ \([^:]*\):\([^:]*\):/\2:\1:/' | \
 	sort | \
 	while IFS=: read TRG PRIO SRC; do
-		ln -sf "$SRC" "$TRG"
+		ln -sf "$SRC" "$ROOT_DIR$TRG"
 	done
