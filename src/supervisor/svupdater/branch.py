f9b1a32af002c95c74a916cbc3726da8e9c63f7e
Martin Matějek <martin.matejek@nic.cz>
2019-11-21 17:26:27 +0100

    fixup! Use generic euci get/set functions

--- src/supervisor/svupdater/branch.py
+++ src/supervisor/svupdater/branch.py
@@ -4,7 +4,7 @@ from euci import EUci, UciExceptionNotFound
 def get_os_branch_or_version():
     """Get OS branch or version from uci."""
     with EUci() as uci:
-        branch = uci.get("updater", "override", "branch", dtype=str, default="deploy")
+        branch = uci.get("updater", "override", "branch", default="deploy")
         # TOS 3.x can only switch between branches, therefore mode is fixed on branch
         mode = "branch"
 
