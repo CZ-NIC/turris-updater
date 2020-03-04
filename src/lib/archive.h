f0e4ec3cc16ef5333e0c3e4aa067d58a702f308a
Karel Kočí <karel.koci@nic.cz>
2020-03-04 16:44:16 +0100

    src/lib/archive: add API documnetation and fix error reset

--- src/lib/archive.h
+++ src/lib/archive.h
@@ -26,17 +26,35 @@
 #include <stdbool.h>
 #include <lua.h>
 
-char *archive_error();
+// Returns error string appropriate for latest error.
+// Returned string is malloc allocated. It is your responsibility to free it.
+char *archive_error() __attribute__((malloc));
 
 // Close provided FILE on fclose of provided FILE
 #define ARCHIVE_AUTOCLOSE (1 << 0)
 
-// TODO attributes
+// Decompress provided FILE. No error is raised if data are not compressed.
+//
+// f: FILE object to read compressed data from
+// flags: optional flags combination
+//   ARCHIVE_AUTOCLOSE: close passed FILE object once returned object is closed
+//
+// Returns FILE object open for reading with decompressed data. On error it
+// returns NULL and you can call archive_error() to receive failure message.
 FILE *decompress(FILE *f, int flags) __attribute__((nonnull));
 
-
+// Unpack standard OpenWrt (ipk) package. Package has two sections. Control files
+// are unpacked to subdirectory control and data are unpacked to subdirectory
+// data.
+//
+// package: path to ipk to be unpacked
+// dir_path: directory to unpack package to
+//
+// Returns true on success or false on failure. On failure you can call
+// archive_error() to receive failure message.
 bool unpack_package(const char *package, const char *dir_path)
 	__attribute__((nonnull));
+
 
 // Create unpack module and inject it into the lua state
 void archive_mod_init(lua_State *L) __attribute__((nonnull));
